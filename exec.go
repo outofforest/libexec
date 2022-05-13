fa34aa838d5f94e84d626adc9613a909b8dc78eb
Wojciech Malota-Wojcik <wojtek@exw.co>
2022-05-13 17:20:40 +0200

    Function for terminating processes

--- exec.go
+++ exec.go
@@ -3,10 +3,12 @@ package libexec
 import (
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
+	"time"
 
 	"github.com/outofforest/logger"
 	"github.com/outofforest/parallel"
@@ -70,4 +72,41 @@ func Exec(ctx context.Context, cmds ...*exec.Cmd) error {
 		}
 	}
 	return nil
+}
+
+// Kill tries to terminate processes gracefully, after timeout it kills them
+func Kill(ctx context.Context, pids []int) error {
+	return parallel.Run(ctx, func(ctx context.Context, spawn parallel.SpawnFn) error {
+		for _, pid := range pids {
+			pid := pid
+			spawn(fmt.Sprintf("%d", pid), parallel.Continue, func(ctx context.Context) error {
+				return parallel.Run(ctx, func(ctx context.Context, spawn parallel.SpawnFn) error {
+					proc, err := os.FindProcess(pid)
+					if err != nil {
+						return err
+					}
+					spawn("waiter", parallel.Exit, func(ctx context.Context) error {
+						_, _ = proc.Wait()
+						return nil
+					})
+					spawn("killer", parallel.Continue, func(ctx context.Context) error {
+						if err := proc.Signal(syscall.SIGTERM); err != nil && !errors.Is(err, os.ErrProcessDone) {
+							return err
+						}
+						select {
+						case <-ctx.Done():
+							return ctx.Err()
+						case <-time.After(20 * time.Second):
+						}
+						if err := proc.Signal(syscall.SIGKILL); err != nil && !errors.Is(err, os.ErrProcessDone) {
+							return err
+						}
+						return nil
+					})
+					return nil
+				})
+			})
+		}
+		return nil
+	})
 }
